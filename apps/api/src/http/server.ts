f79262d56734fb79049a1579b4befa05b3212ac1
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-05-28 10:48:13 -0300

    feat: api docs

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,10 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform
   ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -12,6 +14,23 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Saas rbac',
+      description: 'Fullstack SaaS app w/ multi-tenant RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
