33e5647e33b7c39b35670d11f1c618108ae09f63
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-07-22 18:41:59 -0300

    feat: billing

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -39,6 +39,7 @@ import { acceptInvite } from './routes/invites/accept-invite'
 import { rejectInvite } from './routes/invites/reject-invite'
 import { revokeInvite } from './routes/invites/revoke-invite'
 import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { getOrganizationBilling } from './routes/billing/get-organization-billiting'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -110,6 +111,7 @@ app.register(acceptInvite)
 app.register(rejectInvite)
 app.register(revokeInvite)
 app.register(getPendingInvites)
+app.register(getOrganizationBilling)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP server running on port ${env.SERVER_PORT} 🔥!`)
