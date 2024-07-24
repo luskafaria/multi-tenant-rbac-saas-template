18ee1961886c0197e0ad3ed8107e86c72de7c037
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-07-24 12:39:23 -0300

    fix: eslint server not working

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,7 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: ['@rocketseat/eslint-config/next'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
     'simple-import-sort/imports': 'error',
   },
