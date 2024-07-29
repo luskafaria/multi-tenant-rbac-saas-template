cf1a67baf00b227bc8ce216358b785b9d4a28b33
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-07-29 10:26:09 -0300

    feat: header

---
+++ apps/web/src/components/profile-button.tsx
@@ -0,0 +1,56 @@
+import { auth } from '@/auth/auth'
+import { ChevronDown, LogOut } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+function getInitials(name: string): string {
+  const nameParts = name.split(' ')
+
+  let initials = ''
+
+  for (const part of nameParts) {
+    if (part.length > 0 && initials.length < 2) {
+      initials += part[0].toUpperCase()
+    }
+    if (initials.length >= 2) {
+      break
+    }
+  }
+
+  return initials
+}
+
+export async function ProfileButton() {
+  const { user } = await auth()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center gap-3 outline-none">
+        <div className="flex flex-col items-end">
+          <span className="text-sm font-medium">{user.name}</span>
+          <span className="text-xs text-muted-foreground">{user.email}</span>
+        </div>
+
+        <Avatar>
+          {user.avatarUrl && <AvatarImage src={user.avatarUrl} />}
+          {user.name && (
+            <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+          )}
+        </Avatar>
+        <ChevronDown className="size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem asChild>
+          <a href="/api/auth/sign-out">
+            <LogOut className="mr-4 size-4" />
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
