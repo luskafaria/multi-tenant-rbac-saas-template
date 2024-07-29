d35ce7ffa3938e6feb26022424195067837fed33
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-07-29 09:32:59 -0300

    feat: logged user data

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello world!</div>
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+
+  return <div className="max-w-md">{JSON.stringify(user, null, 2)}</div>
 }
--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,5 +1,25 @@
+import { getProfile } from '@/http/get-profile'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export function isAuthenticated() {
   return !!cookies().get('token')?.value
+}
+
+export async function auth() {
+  const token = cookies().get('token')?.value
+
+  if (!token) {
+    redirect('/api/auth/sign-out')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch (err) {
+    console.warn(err)
+  }
+
+  redirect('/auth/sign-in')
 }
