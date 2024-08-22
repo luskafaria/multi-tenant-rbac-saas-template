685a8d3bc1a1a68f9315dc781973593893aa43c9
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-08-22 11:52:13 -0300

    fix: eslint next setup

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/next', 'plugin:@next/next/recommended'],
   plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
     'simple-import-sort/imports': 'error',
