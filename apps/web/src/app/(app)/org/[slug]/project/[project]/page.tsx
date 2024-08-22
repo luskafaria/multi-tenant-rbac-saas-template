7ec60d121522892a97106e41396d1a5157ad452a
Lucas Faria <lucas.faria.ext@adevinta.com>
2024-08-22 10:08:14 -0300

    feat: project switcher

---
+++ apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/header'
+
+export default function Project() {
+  return (
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p>Project</p>
+      </main>
+    </div>
+  )
+}
